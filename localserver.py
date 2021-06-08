30de86bbcfc4852550cf24e45df53619015b2ec9
tzuleger <74215332+tzuleger@users.noreply.github.com>
2021-06-08 15:57:08 -0500

    Fix

--- localserver.py
+++ localserver.py
@@ -2,6 +2,28 @@ import http.server
 import socketserver
 import socket
 import sys
+from urllib.parse import unquote
+
+class SimpleServer(http.server.SimpleHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def _do_GET(self):
+        if self.path == '/':
+            self.path = '/index.html'
+        return SimpleHTTPRequestHandler.do_GET(self)
+
+    def do_POST(self):
+        self._set_headers()
+        self.data_string = self.rfile.read(int(self.headers['Content-Length']))
+
+        self.send_response(200)
+        self.end_headers()
+
+        data = self.data_string
+        print(unquote(data))
 
 DEFAULT_PORT = 8080
 
@@ -13,12 +35,5 @@ PORT = int(sys.argv[sys.argv.index('-p')+1]) if o_port_num else DEFAULT_PORT
 
 print("Server running on port " + str(PORT) + ", you can connect remotely (same network) by typing \"" + socket.gethostbyname(socket.gethostname()) + ":" + str(PORT) + "\" into the URL of a browser.")
 
-Handler = http.server.SimpleHTTPRequestHandler
-Handler.extensions_map.update(
-{
-    ".js" : "application/javascript",
-    ".wasm" : "application/wasm"
-});
-
-httpd = socketserver.TCPServer(("", PORT), Handler)
-httpd.serve_forever()
+Handler = socketserver.TCPServer(("", PORT), SimpleServer);
+Handler.serve_forever()
